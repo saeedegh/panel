c95a6263ad6f0cc815838d37ebfd66c088617910
saeedegh <saeedegholizade1@gmail.com>
2020-03-05 19:25:44 +0330

    installed axios

--- src/components/ContainerFluid.vue
+++ src/components/ContainerFluid.vue
@@ -1,27 +1,29 @@
 <template>
-  
-    <div class="container-fluid">
-      <div class="row">
-        <div class="card">
-          <div class="card-header card-header-primary pt-4">
-            <h4 class="card-title">لیست اشعار</h4>
-            <a href>
-              <i class="material-icons">playlist_add</i>
-            </a>
-          </div>
-          <div class="card-body">
-           
-          </div>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="card">
+        <div class="card-header card-header-primary pt-4">
+          <h4 class="card-title">لیست اشعار</h4>
+          <a href>
+            <i class="material-icons">playlist_add</i>
+          </a>
         </div>
+        <div class="card-body"></div>
       </div>
     </div>
-   
-  
+  </div>
 </template>
 
 <script>
+
 export default {
-  components: {}
+  components: {},
+  data() {
+    return {};
+  },
+  mounted(){
+
+  }
 };
 </script>
 
