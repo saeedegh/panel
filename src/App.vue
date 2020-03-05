f5049800800c74f5206106aa9c4c078d4128f6a9
saeedegh <saeedegholizade1@gmail.com>
2020-03-05 14:44:00 +0330

    imported new components to App

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <SideBar></SideBar>
+    <MainPanel></MainPanel>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import SideBar from "./components/SideBar.vue";
+import MainPanel from "./components/MainPanel";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    HelloWorld,
+    SideBar,
+    MainPanel
   }
-}
+};
 </script>
 
 <style>
