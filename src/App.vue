17e6a0f88ecb40c1ac1790ada2c759b0497e9bd8
saeedegh <saeedegholizade1@gmail.com>
2020-03-05 15:29:05 +0330

    import style

--- src/App.vue
+++ src/App.vue
@@ -23,12 +23,6 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import '/assets/style.css';
+@import '/assets/style2.css'
 </style>
