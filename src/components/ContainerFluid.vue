854f67d5490216f83cc066254c70a8234bcf283b
saeedegh <saeedegholizade1@gmail.com>
2020-03-06 02:04:54 +0330

    got data

--- src/components/ContainerFluid.vue
+++ src/components/ContainerFluid.vue
@@ -8,7 +8,46 @@
             <i class="material-icons">playlist_add</i>
           </a>
         </div>
-        <div class="card-body"></div>
+        <div class="card-body">
+          <div class="table-responsive">
+            <table class="table">
+              <thead class="text-primary">
+                <th>ردیف</th>
+                <th>عنوان</th>
+                <th>کتاب</th>
+                <th>شاعر</th>
+                <th>وضعیت انتشار</th>
+                <th>عملیات</th>
+              </thead>
+              <tbody id="tbody">
+                <tr v-for="(item, index) in items" :key="index">
+                  <td>{{index+1}}</td>
+                  <td>{{item.title}}</td>
+                  <td v-if="item.book">{{item.book.name}}</td>
+                  <td v-else></td>
+                  <td>{{item.poets[0].name}}</td>
+                  <td v-if="item.isPublished" class="text-success">
+                    <a href="#" class="btn btn-outline-success">منتشر شده</a>
+                  </td>
+                  <td class="text-danger">
+                    <a href="#" class="btn btn-outline-danger">منتشر نشده</a>
+                  </td>
+                  <td>
+                    <a>
+                      <i class="material-icons text-secondary text-danger">delete_forever</i>
+                    </a>
+                    <a href="/manage/poem/edit/?item-id=${item._id}">
+                      <i class="material-icons text-secondary">edit</i>
+                    </a>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div class="row text-center">
+            <div id="pagination"></div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -18,16 +57,19 @@
 export default {
   components: {},
   data() {
-    return {};
+    return {
+      items: []
+    };
   },
   mounted() {
-    // axios
-    //   .get("https://jsonplaceholder.typicode.com/posts")
-    //   .then(data=> console.log(data));
     this.$http
-      .get("https://jsonplaceholder.typicode.com/posts")
-      .then(data => data.json())
-      .then(res => console.log(res.slice(0,10)));
+      .get(
+        "http://localhost:5969/manage/poem/getPagedPoems?page=0&size=14&sortBy=_id&order=Asc")
+      .then(info => info.json())
+      .then(response => {
+        this.items = response.items;
+        console.log(this.items);
+      });
   }
 };
 </script>
