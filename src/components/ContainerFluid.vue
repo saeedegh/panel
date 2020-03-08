ef0e2b0fc40128a89187fe3498edcbe6d3de7faf
saeedegh <saeedegholizade1@gmail.com>
2020-03-09 01:36:42 +0330

    edited table

--- src/components/ContainerFluid.vue
+++ src/components/ContainerFluid.vue
@@ -13,39 +13,38 @@
             <img src="../assets/img/progress.gif" alt="progressbar" height="50" width="50" />
           </div>
           <div class="table-responsive">
-            <table class="table">
-              <thead class="text-primary">
-                <th>ردیف</th>
-                <th>عنوان</th>
-                <th>کتاب</th>
-                <th>شاعر</th>
-                <th>وضعیت انتشار</th>
-                <th>عملیات</th>
-              </thead>
-              <tbody id="tbody">
-                <tr v-for="(item, index) in items" :key="index">
-                  <td>{{index+1}}</td>
-                  <td>{{item.title}}</td>
-                  <td v-if="item.book">{{item.book.name}}</td>
-                  <td v-else></td>
-                  <td>{{item.poets[0].name}}</td>
-                  <td v-if="item.isPublished" class="text-success">
-                    <a href="#" class="btn btn-outline-success">منتشر شده</a>
-                  </td>
-                  <td class="text-danger">
-                    <a href="#" class="btn btn-outline-danger">منتشر نشده</a>
-                  </td>
-                  <td>
-                    <a>
-                      <i class="material-icons text-secondary text-danger">delete_forever</i>
-                    </a>
-                    <a href="/manage/poem/edit/?item-id=${item._id}">
-                      <i class="material-icons text-secondary">edit</i>
-                    </a>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+            <b-table
+              id="tbody"
+              :items="items"
+              :per-page="perPage"
+              :current-page="currentPage"
+              :fields="fields"
+              small
+            >
+              <template v-slot:cell(ردیف)="data">{{14*(currentPage-1)+ data.index + 1 }}</template>
+              <template v-slot:cell(عنوان)="data">{{ data.item.title}}</template>
+              <template v-slot:cell(کتاب)="data">
+                <div v-if="data.item.book">{{ data.item.book.name}}</div>
+                <div v-else></div>
+              </template>
+              <template v-slot:cell(شاعر)="data">{{ data.item.poets[0].name}}</template>
+              <template v-slot:cell(وضعیت)="data">
+                <div v-if="data.item.ispublished" href="#" class="btn btn-outline-success">منتشر شده</div>
+                <div v-else href="#" class="btn btn-outline-danger">منتشر نشده</div>
+              </template>
+              <template v-slot:cell(عملیات)="data">
+                <a>
+                  <i class="material-icons text-secondary text-danger">delete_forever</i>
+                </a>
+                <a :href="'http://localhost:5969/manage/poem/edit/?item-id=' + data.item._id">
+                  <i class="material-icons text-secondary">edit</i>
+                </a>
+              </template>
+
+              <template v-slot:cell()="data">
+                <i>{{ data.value }}</i>
+              </template>
+            </b-table>
           </div>
           <div class="row text-center">
             <b-pagination
@@ -67,37 +66,29 @@ export default {
   data() {
     return {
       items: [],
+      fields: ["ردیف", "عنوان", "کتاب", "شاعر", "وضعیت", "عملیات"],
       dataLoaded: false,
       response: {},
-      pageNo:0,
-      perPage:14,
-
-
-    }
+      currentPage: 1,
+      perPage: 14
+    };
   },
-  computed:{
-      rows(){ return this.response.totalCount},
-      currentPage(){ return this.pageNo +1}
+  computed: {
+    rows() {
+      return this.response.totalCount;
+    }
   },
 
   mounted() {
     this.dataLoaded = false;
     this.$http
-      .get(
-        `http://localhost:5969/manage/poem/getPagedPoems?page=${this.pageNo}&size=14&sortBy=_id&order=Asc`
-      )
+      .get(`http://localhost:5969/manage/poem/getPagedPoems`)
       .then(info => info.json())
       .then(response => {
-        this.response = response; 
+        this.response = response;
         this.items = response.items;
-        
       });
     this.dataLoaded = true;
-    console.log(  typeof(this.pageNo));
-  
-    
-    console.log('current:' , this.currentPage);
-    
   }
 };
 </script>
